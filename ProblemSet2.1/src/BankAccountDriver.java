5c509f0a7e9801bdcd3bba057b9db87c2f609f50
David O'Sullivan <david.osullivan5@students.ittralee.ie>
2017-10-18 15:44:38 +0100

    updated stuff

--- ProblemSet2.1/src/BankAccountDriver.java
+++ ProblemSet2.1/src/BankAccountDriver.java
@@ -6,24 +6,37 @@ public class BankAccountDriver {
     public static void main(String[] args) {
 
         String name="";
-        int accountNo=0;
+        BankAccount[] accountList = new BankAccount[100];
+        int accountNo=0, i=0, dialogOption;
         double balance = 0.0;
-
-        name = JOptionPane.showInputDialog("Please enter your name");
-        accountNo = Integer.parseInt(JOptionPane.showInputDialog("Please enter your acc/no"));
-        balance = Double.parseDouble(JOptionPane.showInputDialog("What is your expected balance"));
+        boolean intention=true;
 
         Font myFont = new Font(Font.MONOSPACED, Font.PLAIN, 20);
         JTextArea myTextArea = new JTextArea();
         myTextArea.setFont(myFont);
 
-        BankAccount userAccount = new BankAccount(name, accountNo, balance);
+        while(intention) {
+            name = JOptionPane.showInputDialog("Please enter your name");
+            accountNo = Integer.parseInt(JOptionPane.showInputDialog("Please enter your acc/no"));
+            balance = Double.parseDouble(JOptionPane.showInputDialog("What is your expected balance"));
 
-        BankAccount myEmptyBank = new BankAccount();
+            BankAccount userAccount = new BankAccount(name, accountNo, balance);
 
-        myTextArea.append(userAccount.toString());
+            accountList[i] = userAccount;
+            i++;
 
+            myTextArea.append(userAccount.toString() + "\n\n");
+
+            dialogOption = JOptionPane.showConfirmDialog(null,
+                    "Would you like to enter another account?",
+                    "Accounts", JOptionPane.YES_NO_OPTION);
+
+            if(dialogOption==JOptionPane.NO_OPTION)
+                intention = false;
+        }
         JOptionPane.showMessageDialog(null, myTextArea);
+
+        System.exit(0);
     }
 
 
